732acf4aabeb7829a56d029946332e1e39c0a383
leomoraiscam <leozonnn@gmail.com>
2020-06-08 17:25:31 -0500

    implementando as configurações de migrations

--- src/config/database.js
+++ src/config/database.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   dialect: 'postgres',
   host: process.env.HOST,
-  username: process.env.USERNAME,
+  username: 'postgres',
   password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  database: 'sequelize_example',
   define: {
     timestamps: true,
     underscored: true,
