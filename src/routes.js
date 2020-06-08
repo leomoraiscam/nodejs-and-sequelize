ec37c90a74b4d676b8a04d04992879636975c7d6
leomoraiscam <leozonnn@gmail.com>
2020-06-08 18:04:00 -0500

    Criando o primeito model e o primeito controller de usuario e configuração do model com o banco de dados

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,8 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/user', (req, res) => {
-  return res.json({
-    name: 'Leonardo Morais',
-    projeto: 'Node.Js com Sequelize',
-  });
-});
+const UserController = require('./controller/UserController');
+
+routes.post('/users', UserController.store);
 
 module.exports = routes;
