457210b713bcdd5833bb4cacdf4af57c4cd17fb7
leomoraiscam <leozonnn@gmail.com>
2020-06-08 17:38:28 -0500

    adicionando um camppo a tabela de users

---
+++ src/database/migrations/20200608223014-add_age_field_to_users.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'age', {
+      type: Sequelize.INTEGER,
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'age');
+  },
+};
