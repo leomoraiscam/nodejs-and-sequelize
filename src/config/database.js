5950c525abd1975dde8b34d57b725775eb4f57da
leomoraiscam <leozonnn@gmail.com>
2020-06-08 15:44:45 -0500

    Configurado as variaveis ambiente e conectando com o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.HOST,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
