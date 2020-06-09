cfd7b761f8b868e4561c6e483ffc96e2c4278430
leomoraiscam <leozonnn@gmail.com>
2020-06-09 18:24:23 -0500

    Utilzando associações e entrando mais a fundo em relacionamentos de muitos para muitos

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,14 @@ const routes = express.Router();
 
 const UserController = require('./controller/UserController');
 const AddressController = require('./controller/AddressController');
+const TechController = require('./controller/TechController');
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.get('/users/:user_id/addresses', AddressController.index);
 routes.post('/users/:user_id/addresses', AddressController.store);
+routes.get('/users/:user_id/techs', TechController.index);
+routes.post('/users/:user_id/techs', TechController.store);
+routes.delete('/users/:user_id/techs', TechController.delete);
 
 module.exports = routes;
