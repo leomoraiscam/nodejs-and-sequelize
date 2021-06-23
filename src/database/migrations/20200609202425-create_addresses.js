41350423905a64dbc03e28d59f6450a8ac7ace63
lmorais <leozonnn@gmail.com>
2021-06-23 00:24:48 -0300

    modify nameclature in address entity

--- src/database/migrations/20200609202425-create_addresses.js
+++ src/database/migrations/20200609202425-create_addresses.js
@@ -14,7 +14,7 @@ module.exports = {
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       },
-      zipcode: {
+      zip_code: {
         type: Sequelize.STRING,
         allowNull: false,
       },
