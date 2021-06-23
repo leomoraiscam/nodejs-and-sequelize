eb92e5cefbd376619b5c6346295b6eb4caae5a9f
lmorais <leozonnn@gmail.com>
2021-06-23 00:25:51 -0300

    modify address entity

--- src/app/models/Address.js
+++ src/app/models/Address.js
@@ -4,12 +4,21 @@ class Address extends Model {
   static init(sequelize) {
     super.init(
       {
-        zipcode: DataTypes.STRING,
+        zip_code: DataTypes.STRING,
         street: DataTypes.STRING,
         number: DataTypes.INTEGER,
+        createdAt: {
+          type: DataTypes.DATE,
+          field: 'created_at',
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          field: 'updated_at',
+        },
       },
       {
         sequelize,
+        tableName: 'addresses',
       }
     );
 
