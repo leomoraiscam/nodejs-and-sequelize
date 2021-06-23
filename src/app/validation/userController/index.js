ac7102cb6640cc0bdf50241d35233ac70a5baf59
lmorais <leozonnn@gmail.com>
2021-06-23 00:06:29 -0300

    nameclarure in http layer

--- src/app/validation/userController/index.js
+++ src/app/validation/userController/index.js
@@ -1,18 +1,18 @@
 const yup = require('yup');
 
 module.exports = {
-  async store(req, res, next) {
+  async store(request, response, next) {
     try {
       const schema = yup.object().shape({
         name: yup.string().required(),
         email: yup.string().email().required(),
       });
 
-      await schema.validate(req.body, { abortEarly: false });
+      await schema.validate(request.body, { abortEarly: false });
 
       return next();
     } catch (error) {
-      return res
+      return response
         .status(400)
         .json({ error: 'Validation fail', message: error.inner });
     }
